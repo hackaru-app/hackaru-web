d4dc256b24055f0442872979a864afae819a91c0
ktmouk <ktmouk@gmail.com>
2019-05-20 15:17:07 +0900

    Improve ga stub

--- plugins/vue-analytics.js
+++ plugins/vue-analytics.js
@@ -1,17 +1,10 @@
 import Vue from 'vue';
 import VueAnalytics from 'vue-analytics';
 
+const stub = { event: () => {}, set: () => {} };
+
 export default async ({ app }) => {
   const id = app.$env.GOOGLE_ANALYTICS_TRACKING_ID;
-
-  // Set dummy $ga if don't use GoogleAnalytics
-  if (!id) {
-    Vue.prototype.$ga = {
-      event: () => {},
-      set: () => {}
-    };
-    return;
-  }
-
+  if (!id) return stub;
   Vue.use(VueAnalytics, { router: app.router, id });
 };
