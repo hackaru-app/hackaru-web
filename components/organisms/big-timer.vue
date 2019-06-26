3784eda2e826ba29f17694e15e5699c06254b35b
ktmouk <ktmouk@gmail.com>
2019-06-26 10:41:37 +0900

    Improve big-timer

--- components/organisms/big-timer.vue
+++ components/organisms/big-timer.vue
@@ -1,4 +1,4 @@
-<i18n src="@/assets/locales/pages/index.json" />
+<i18n src="@/assets/locales/components/organisms/big-timer.json" />
 
 <template>
   <form class="big-timer" @submit.prevent="submit">
@@ -6,29 +6,35 @@
       :started-at="startedAt"
       :class="['duration', { stopped: !startedAt }]"
     />
-    <base-button
-      v-if="!startedAt"
-      type="submit"
-      class="is-primary control-button play"
-    >
-      <icon name="play-icon" />
-    </base-button>
-    <base-button v-else type="submit" class="is-danger control-button stop">
-      <icon name="square-icon" />
-    </base-button>
     <div class="form">
-      <div class="form-item">
-        <project-name v-bind="project" />
-      </div>
-      <div class="form-item">
+      <div class="form-content">
+        <div class="project-wrapper">
+          <project-name v-bind="project" />
+        </div>
         <input
           v-model="description"
+          :placeholder="$t('description')"
           type="text"
           class="description"
-          placeholder="作業内容や備考を入力..."
           autofocus
         />
       </div>
+      <base-button
+        v-tooltip="$t('start')"
+        v-if="!startedAt"
+        type="submit"
+        class="is-primary control-button start"
+      >
+        <icon name="play-icon" />
+      </base-button>
+      <base-button
+        v-tooltip="$t('stop')"
+        v-else
+        type="submit"
+        class="is-danger control-button stop"
+      >
+        <icon name="square-icon" />
+      </base-button>
     </div>
   </form>
 </template>
@@ -99,6 +105,7 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin-top: -30px;
 }
 .duration {
   font-size: 76px;
@@ -110,68 +117,117 @@ export default {
 }
 .control-button {
   flex-shrink: 0;
-  width: 63px;
-  height: 63px;
-  margin-top: 15px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  padding: 0;
+  width: 65px;
+  height: 100%;
+  border-radius: 0 3px 3px 0;
   pointer-events: auto;
 }
-.control-button.play {
-  .icon {
-    width: 30px;
-    height: 30px;
-    padding-left: 5px;
-  }
+.control-button .icon {
+  width: 24px;
+  height: 24px;
 }
-.control-button.stop {
-  .icon {
-    width: 25px;
-    height: 25px;
-  }
+.control-button.start .icon {
+  padding-left: 3px;
 }
 .form {
-  margin-top: 50px;
-  max-width: 400px;
-  pointer-events: auto;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  border: 1px $border solid;
-  border-left: 0;
-  border-right: 0;
-}
-.form-item {
-  display: flex;
+  width: calc(100vw - #{$side-bar-min-width});
+  max-width: 700px;
   justify-content: center;
-  height: 65px;
+  margin-top: 20px;
+  margin-bottom: 40px;
+  pointer-events: auto;
+  display: flex;
+  border-radius: 5px;
+  height: 64px;
+  padding: 0 50px;
+  box-sizing: border-box;
+}
+.form-content {
+  display: flex;
+  flex: 1;
+  width: 100%;
+  border: 1px $border solid;
+  border-right: 0;
+  border-radius: 3px 0 0 3px;
 }
 .project-name {
   display: flex;
   height: 100%;
-  padding-right: 10px;
+  max-width: 120px;
+  padding-right: 30px;
+  padding-left: 25px;
+  border-right: 1px $border solid;
   align-items: center;
 }
 .description {
-  display: flex;
-  align-items: center;
   line-height: 1;
   border: 0;
-  height: 100%;
   width: 100%;
-  text-align: center;
-  border-top: 1px $border solid;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 0 25px;
+  display: flex;
   background: none;
 }
 @include mq(small) {
-  .form-item {
+  .big-timer {
+    margin-top: 0;
+  }
+  .form {
+    height: auto;
+    padding: 0;
+    justify-content: flex-start;
+    margin: 0;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 100%;
+  }
+  .project-name,
+  .description {
     height: 70px;
   }
+  .description {
+    text-align: center;
+  }
+  .project-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    border-bottom: 1px $border solid;
+  }
+  .project-name {
+    max-width: none;
+    border: 0;
+  }
+  .form-content {
+    order: 1;
+    flex-direction: column;
+    border-radius: 0;
+    border-left: 0;
+    border-right: 0;
+  }
+  .control-button {
+    width: 65px;
+    height: 65px;
+    border-radius: 50%;
+    padding: 0;
+    margin-bottom: 50px;
+  }
+  .control-button .icon {
+    width: 25px;
+    height: 25px;
+  }
+  .control-button.start .icon {
+    padding-left: 5px;
+    width: 28px;
+    height: 28px;
+  }
   .duration {
-    font-size: 64px;
+    font-size: 72px;
   }
 }
 </style>
