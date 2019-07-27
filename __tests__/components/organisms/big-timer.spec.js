bd47d26de4733c38928673137fe0e1af7ce16fce
ktmouk <ktmouk@gmail.com>
2019-07-27 20:39:38 +0900

    big-timerのsubmitのtestを追加

--- __tests__/components/organisms/big-timer.spec.js
+++ __tests__/components/organisms/big-timer.spec.js
@@ -1,3 +1,4 @@
+import MockDate from 'mockdate';
 import { Store } from 'vuex-mock-store';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import BigTimer from '@/components/organisms/big-timer';
@@ -5,6 +6,8 @@ import Tooltip from '@/plugins/v-tooltip';
 
 describe('BigTimer', () => {
   let wrapper;
+
+  MockDate.set('2019-01-31T01:23:45');
 
   const localVue = createLocalVue();
   localVue.directive('tooltip', Tooltip);
@@ -43,7 +46,6 @@ describe('BigTimer', () => {
   describe('when select project and timer is not working', () => {
     beforeEach(() => {
       wrapper = factory();
-      wrapper.set;
       wrapper.find('.nav-modal').vm.$emit('close', {
         project: {
           id: 1,
@@ -53,15 +55,11 @@ describe('BigTimer', () => {
       });
     });
 
-    it('update selected-project', () => {
-      expect(wrapper.find('.selected-project').attributes().name).toBe(
-        'Review'
-      );
-      expect(wrapper.find('.selected-project').attributes().color).toBe('#ff0');
-    });
-
     it('does not dispatch', () => {
-      expect($store.dispatch).not.toHaveBeenCalled();
+      expect($store.dispatch).not.toHaveBeenCalledWith(
+        'activities/update',
+        expect.any(Object)
+      );
     });
   });
 
@@ -88,6 +86,44 @@ describe('BigTimer', () => {
         id: 1,
         description: 'Review my tasks',
         projectId: 1
+      });
+    });
+  });
+
+  describe('when submit and timer is not working', () => {
+    beforeEach(() => {
+      wrapper = factory();
+      wrapper.find('.nav-modal').vm.$emit('close', {
+        project: {
+          id: 1,
+          name: 'Review',
+          color: '#ff0'
+        }
+      });
+      wrapper.find('.description').setValue('Review my tasks');
+      wrapper.find('.form').trigger('submit');
+    });
+
+    it('dispatch activities/add', () => {
+      expect($store.dispatch).toHaveBeenCalledWith('activities/add', {
+        description: 'Review my tasks',
+        projectId: 1,
+        startedAt: `${new Date()}`
+      });
+    });
+  });
+
+  describe('when submit and timer is working', () => {
+    beforeEach(() => {
+      wrapper = factory();
+      wrapper.setData({ id: 1 });
+      wrapper.find('.form').trigger('submit');
+    });
+
+    it('dispatch activities/update', () => {
+      expect($store.dispatch).toHaveBeenCalledWith('activities/update', {
+        id: 1,
+        stoppedAt: `${new Date()}`
       });
     });
   });
