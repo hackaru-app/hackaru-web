4826847ffcae04f71c6f17eb5f01e15ca5382c80
ktmouk <ktmouk@gmail.com>
2019-08-22 16:31:45 +0900

    suggestionのtestを追加

---
+++ __tests__/store/suggestions/actions.spec.js
@@ -0,0 +1,27 @@
+import { actions } from '@/store/suggestions';
+
+describe('Actions', () => {
+  describe('when dispatch fetch', () => {
+    const commit = jest.fn();
+    const dispatch = jest.fn(() => ({ data: {} }));
+
+    beforeEach(() => {
+      actions.fetch({ commit, dispatch }, 'query');
+    });
+
+    it('dispatch auth-api/request', () => {
+      expect(dispatch).toHaveBeenCalledWith(
+        'auth-api/request',
+        {
+          url: '/v1/suggestions',
+          params: { q: 'query' }
+        },
+        { root: true }
+      );
+    });
+
+    it('commit SET_SUGGESTIONS', () => {
+      expect(commit).toHaveBeenCalledWith('SET_SUGGESTIONS', {});
+    });
+  });
+});
