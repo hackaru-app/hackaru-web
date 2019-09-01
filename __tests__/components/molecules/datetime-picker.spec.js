4829ed927776d704963e8c03901ae1880ccd089b
ktmouk <ktmouk@gmail.com>
2019-09-01 16:31:06 +0900

    datetime-pickerを修正

--- __tests__/components/molecules/datetime-picker.spec.js
+++ __tests__/components/molecules/datetime-picker.spec.js
@@ -1,6 +1,5 @@
 import MockDate from 'mockdate';
 import { shallowMount } from '@vue/test-utils';
-import { parse } from 'date-fns';
 import DatetimePicker from '@/components/molecules/datetime-picker';
 
 describe('DatetimePicker', () => {
@@ -17,21 +16,19 @@ describe('DatetimePicker', () => {
     });
 
     it('emit input with current date', () => {
-      expect(wrapper.emitted('input')[0]).toEqual([`${new Date()}`]);
+      expect(wrapper.emitted('input')[0]).toEqual(['2019-01-31 01:23:45']);
     });
   });
 
   describe('when focus date and date already inputted', () => {
     beforeEach(() => {
       wrapper = factory();
-      wrapper.setProps({ value: `${parse('2018-03-03T01:23:45')}` });
+      wrapper.setProps({ value: '2018-03-03T01:23:45' });
       wrapper.find('.date').trigger('focus');
     });
 
     it('emit input with inputted date', () => {
-      expect(wrapper.emitted('input')[0]).toEqual([
-        `${parse('2018-03-03T01:23:45')}`
-      ]);
+      expect(wrapper.emitted('input')[0]).toEqual(['2018-03-03 01:23:45']);
     });
   });
 
@@ -42,21 +39,19 @@ describe('DatetimePicker', () => {
     });
 
     it('emit input with current date', () => {
-      expect(wrapper.emitted('input')[0]).toEqual([`${new Date()}`]);
+      expect(wrapper.emitted('input')[0]).toEqual(['2019-01-31 01:23:45']);
     });
   });
 
   describe('when focus time but time already inputted', () => {
     beforeEach(() => {
       wrapper = factory();
-      wrapper.setProps({ value: `${parse('2018-03-03T01:23:45')}` });
+      wrapper.setProps({ value: '2018-03-03T01:23:45' });
       wrapper.find('.time').trigger('focus');
     });
 
     it('emit input with inputted date', () => {
-      expect(wrapper.emitted('input')[0]).toEqual([
-        `${parse('2018-03-03T01:23:45')}`
-      ]);
+      expect(wrapper.emitted('input')[0]).toEqual(['2018-03-03 01:23:45']);
     });
   });
 
