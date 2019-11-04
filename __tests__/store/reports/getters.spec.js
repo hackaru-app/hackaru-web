b5a50539f2ef1850c8929275e77a13324480314d
ktmouk <ktmouk@gmail.com>
2019-11-04 12:14:26 +0900

    reportのgettersのtestを修正

--- __tests__/store/reports/getters.spec.js
+++ __tests__/store/reports/getters.spec.js
@@ -1,18 +1,11 @@
 import { getters } from '@/store/reports';
-import { parseISO } from 'date-fns';
 
 describe('Getters', () => {
   let result;
 
   describe('when call projects', () => {
     const state = {
-      projects: [
-        {
-          id: 1,
-          name: 'Development',
-          color: '#ff0'
-        }
-      ]
+      projects: []
     };
 
     beforeEach(() => {
@@ -20,52 +13,27 @@ describe('Getters', () => {
     });
 
     it('returns projects', () => {
-      expect(result).toEqual([
-        {
-          id: 1,
-          name: 'Development',
-          color: '#ff0'
-        }
-      ]);
+      expect(result).toEqual([]);
     });
   });
 
-  describe('when call summary', () => {
+  describe('when call totals', () => {
     const state = {
-      summary: [
-        {
-          projectId: 1,
-          duration: 100,
-          date: '2019-01-01T00:00:00'
-        },
-        {
-          projectId: 1,
-          duration: 200,
-          date: '2019-01-02T00:00:00'
-        },
-        {
-          projectId: 2,
-          duration: 200,
-          date: '2019-01-02T00:00:00'
-        }
-      ]
+      totals: {}
     };
 
     beforeEach(() => {
-      result = getters.summary(state);
+      result = getters.totals(state);
     });
 
     it('returns summary', () => {
-      expect(result).toEqual({
-        1: 300,
-        2: 200
-      });
+      expect(result).toEqual({});
     });
   });
 
-  describe('when call empty and summary is empty', () => {
+  describe('when call empty and totals is empty', () => {
     const state = {
-      summary: []
+      totals: {}
     };
 
     beforeEach(() => {
@@ -79,13 +47,9 @@ describe('Getters', () => {
 
   describe('when call empty and summary is not empty', () => {
     const state = {
-      summary: [
-        {
-          projectId: 1,
-          duration: 100,
-          date: '2019-01-01T00:00:00'
-        }
-      ]
+      totals: {
+        1: 100
+      }
     };
 
     beforeEach(() => {
@@ -94,88 +58,6 @@ describe('Getters', () => {
 
     it('returns false', () => {
       expect(result).toBe(false);
-    });
-  });
-
-  describe('when call barChartLabels and period is hour', () => {
-    const state = {
-      period: 'hour'
-    };
-
-    beforeEach(() => {
-      result = getters.barChartLabels(state);
-    });
-
-    it('returns labels', () => {
-      expect(result).toEqual([
-        '0:00',
-        '1:00',
-        '2:00',
-        '3:00',
-        '4:00',
-        '5:00',
-        '6:00',
-        '7:00',
-        '8:00',
-        '9:00',
-        '10:00',
-        '11:00',
-        '12:00',
-        '13:00',
-        '14:00',
-        '15:00',
-        '16:00',
-        '17:00',
-        '18:00',
-        '19:00',
-        '20:00',
-        '21:00',
-        '22:00',
-        '23:00'
-      ]);
-    });
-  });
-
-  describe('when call barChartLabels and period is day', () => {
-    const state = {
-      period: 'day',
-      start: parseISO('2019-01-01'),
-      end: parseISO('2019-01-03')
-    };
-
-    beforeEach(() => {
-      result = getters.barChartLabels(state);
-    });
-
-    it('returns labels', () => {
-      expect(result).toEqual(['01', '02', '03']);
-    });
-  });
-
-  describe('when call barChartLabels and period is month', () => {
-    const state = {
-      period: 'month'
-    };
-
-    beforeEach(() => {
-      result = getters.barChartLabels(state);
-    });
-
-    it('returns labels', () => {
-      expect(result).toEqual([
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec'
-      ]);
     });
   });
 
@@ -193,40 +75,19 @@ describe('Getters', () => {
           color: '#f00'
         }
       ],
-      summary: [
-        {
-          projectId: 1,
-          duration: 100,
-          date: '2019-01-01'
-        },
-        {
-          projectId: 1,
-          duration: 200,
-          date: '2019-01-02'
-        },
-        {
-          projectId: 2,
-          duration: 0,
-          date: '2019-01-01'
-        },
-        {
-          projectId: 2,
-          duration: 100,
-          date: '2019-01-02'
-        }
-      ]
-    };
-
-    const mockGetters = {
-      barChartLabels: ['01/01', '01/02']
+      labels: ['Jan', 'Feb'],
+      sums: {
+        1: [100, 200],
+        2: [300, 400]
+      }
     };
 
     beforeEach(() => {
-      result = getters.barChartData(state, mockGetters);
+      result = getters.barChartData(state);
     });
 
     it('returns labels correctly', () => {
-      expect(result.labels).toEqual(['01/01', '01/02']);
+      expect(result.labels).toEqual(['Jan', 'Feb']);
     });
 
     it('returns datasets correctly', () => {
@@ -239,7 +100,7 @@ describe('Getters', () => {
         {
           label: 'Review',
           backgroundColor: '#f00',
-          data: [0, 100]
+          data: [300, 400]
         }
       ]);
     });
@@ -257,33 +118,25 @@ describe('Getters', () => {
           id: 2,
           name: 'Review',
           color: '#f00'
-        },
-        {
-          id: 3,
-          name: 'Coding',
-          color: '#f0f'
         }
-      ]
-    };
-
-    const mockGetters = {
-      summary: {
+      ],
+      totals: {
         1: 100,
         2: 200
       }
     };
 
     beforeEach(() => {
-      result = getters.doughnutChartData(state, mockGetters);
+      result = getters.doughnutChartData(state);
     });
 
     it('returns chart data', () => {
       expect(result).toEqual({
-        labels: ['Development', 'Review', 'Coding'],
+        labels: ['Development', 'Review'],
         datasets: [
           {
-            data: [100, 200, 0],
-            backgroundColor: ['#ff0', '#f00', '#f0f']
+            data: [100, 200],
+            backgroundColor: ['#ff0', '#f00']
           }
         ]
       });
