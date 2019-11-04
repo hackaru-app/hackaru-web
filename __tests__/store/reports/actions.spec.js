18a4af9325375a3317912c82268252414ed7a5ef
ktmouk <ktmouk@gmail.com>
2019-11-04 22:56:47 +0900

    reportのactionのtestを追加

--- __tests__/store/reports/actions.spec.js
+++ __tests__/store/reports/actions.spec.js
@@ -55,4 +55,40 @@ describe('Actions', () => {
       });
     });
   });
+
+  describe('when dispatch fetchPdf', () => {
+    let result;
+
+    const dispatch = jest.fn(() => ({ data: '%PDF' }));
+
+    beforeEach(async () => {
+      result = await actions.fetchPdf(
+        { dispatch },
+        {
+          start: parseISO('2019-01-01T00:00:00'),
+          end: parseISO('2019-01-03T00:00:00')
+        }
+      );
+    });
+
+    it('dispatch auth-api/request', () => {
+      expect(dispatch).toHaveBeenCalledWith(
+        'auth-api/request',
+        {
+          url: '/v1/report.pdf',
+          responseType: 'blob',
+          params: {
+            start: parseISO('2019-01-01T00:00:00'),
+            end: parseISO('2019-01-03T00:00:00'),
+            timeZone: 'America/New_York'
+          }
+        },
+        { root: true }
+      );
+    });
+
+    it('retuns data', () => {
+      expect(result).toBe('%PDF');
+    });
+  });
 });
