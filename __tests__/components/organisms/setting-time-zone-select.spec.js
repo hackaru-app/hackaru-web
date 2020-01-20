ce9ea90021c32dd90a8b919e81e90043175b2b97
ktmouk <ktmouk@gmail.com>
2020-01-20 22:07:51 +0900

    setting-time-zone-selectのtestを追加

---
+++ __tests__/components/organisms/setting-time-zone-select.spec.js
@@ -0,0 +1,37 @@
+import { Store } from 'vuex-mock-store';
+import { shallowMount } from '@vue/test-utils';
+import SettingTimeZoneSelect from '@/components/organisms/setting-time-zone-select';
+
+describe('SettingTimeZoneSelect', () => {
+  let wrapper;
+
+  const $store = new Store({
+    getters: {
+      'user/timeZone': 'Etc/UTC'
+    }
+  });
+
+  const factory = () =>
+    shallowMount(SettingTimeZoneSelect, {
+      mocks: {
+        $store
+      }
+    });
+
+  beforeEach(() => {
+    $store.reset();
+  });
+
+  describe('when select', () => {
+    beforeEach(() => {
+      wrapper = factory();
+      wrapper.find({ ref: 'base-select' }).vm.$emit('change', 'Asia/Tokyo');
+    });
+
+    it('change user/update', () => {
+      expect($store.dispatch).toHaveBeenCalledWith('user/update', {
+        timeZone: 'Asia/Tokyo'
+      });
+    });
+  });
+});
