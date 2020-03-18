21443d4e733b5e6c52dbfbfc076326cd5141434d
ktmouk <ktmouk@gmail.com>
2020-03-18 21:30:54 +0900

    user-scalableをnoに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ module.exports = {
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width,initial-scale=1,viewport-fit=cover'
+        content:
+          'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover'
       },
       {
         hid: 'description',
