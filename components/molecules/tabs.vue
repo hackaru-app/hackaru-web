c691b0ba979dd332cfd2d044769543d587061fc4
ktmouk <ktmouk@gmail.com>
2020-03-20 17:44:35 +0900

    tabのスクロール部分にグラデーションを追加

--- components/molecules/tabs.vue
+++ components/molecules/tabs.vue
@@ -1,14 +1,11 @@
 <template>
-  <ul class="tabs">
-    <li
-      v-for="(item, index) in items"
-      :key="index"
-      :class="{ selected: isSelected(index) }"
-      @click="change(index)"
-    >
-      {{ item }}
-    </li>
-  </ul>
+  <div class="tabs">
+    <ul>
+      <li v-for="(item, index) in items" :key="index" @click="change(index)">
+        <span :class="{ selected: isSelected(index) }">{{ item }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -37,32 +34,50 @@ export default {
 <style scoped lang="scss">
 .tabs {
   display: flex;
+  position: relative;
+  width: 100%;
+  &:after {
+    position: absolute;
+    right: 0;
+    display: block;
+    content: '';
+    width: 100px;
+    height: 100%;
+    background: linear-gradient(to right, transparent, $white);
+    pointer-events: none;
+  }
+}
+.tabs ul {
+  display: flex;
+  margin: 0;
+  padding: 0;
   overflow-x: scroll;
   -webkit-overflow-scrolling: touch;
   -ms-overflow-style: none;
-  margin: 0;
-  padding: 0;
+  padding-right: 100px;
 }
 .tabs li {
   box-sizing: border-box;
   flex-shrink: 0;
-  display: flex;
-  border-bottom: 2px $border solid;
   list-style-type: none;
   list-style-position: inside;
+  &:last-child {
+    padding-right: 70px;
+  }
+}
+.tabs li span {
+  display: flex;
+  border-bottom: 2px $border solid;
   padding: 10px 15px;
   padding-top: 2px;
   align-items: center;
   color: $text-light;
   transition: all 0.2s ease;
-}
-.tabs li.selected,
-.tabs li:hover {
-  cursor: pointer;
-  border-bottom-color: $cyan;
-  color: $text;
-}
-.tabs li:active {
-  color: $text-lighter;
+  &.selected,
+  &:hover {
+    cursor: pointer;
+    border-bottom-color: $cyan;
+    color: $text;
+  }
 }
 </style>
