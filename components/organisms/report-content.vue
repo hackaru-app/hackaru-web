c3089ee2ecb824ed3b3384a7389e9e7ebe476f0f
ktmouk <ktmouk@gmail.com>
2020-03-22 23:08:22 +0900

    スマートフォンの背景を修正

--- components/organisms/report-content.vue
+++ components/organisms/report-content.vue
@@ -242,6 +242,17 @@ export default {
     padding: 0;
     margin: 0;
   }
+  .doughnut-chart-wrapper {
+    display: flex;
+    align-self: center;
+    justify-content: center;
+    padding: 30px 0;
+    order: 0;
+    margin: 0;
+    border: 0;
+    box-shadow: none;
+    background-color: $background;
+  }
   .doughnut-chart {
     padding: 0 0;
     align-self: center;
@@ -259,19 +270,10 @@ export default {
     margin: 0;
     flex-direction: column;
   }
-  .doughnut-chart-wrapper {
-    display: flex;
-    align-self: center;
-    justify-content: center;
-    padding: 30px 0;
-    order: 0;
-    margin: 0;
-    border: 0;
-    box-shadow: none;
-  }
   .details {
     border: 0;
     box-shadow: none;
+    background-color: $background;
   }
   .details article {
     height: 65px;
